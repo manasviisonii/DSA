1cde72ad9c6a2cf4eb9ec39acee5bfb6496d0b85
Manasvi Soni <163575851+manasviisonii@users.noreply.github.com>
2025-09-10 11:26:12 +0530

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int original = n;
+        int rev = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        if (original == rev) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is NOT a Palindrome");
+        }
+    }
+}
